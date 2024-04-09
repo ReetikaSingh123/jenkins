c459fc2256b27f81ead198cbee20d0d40cabd91f
reetikasingh <reetika.singh@nagarro.com>
2024-04-09 15:23:22 +0530

    commit

---
+++ Jenkin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Jenkin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
